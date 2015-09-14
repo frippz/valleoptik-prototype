9debd0f9123a11af51ee4a5e3595849ac1cea905
Fredrik Frodlund <frippz@me.com>
2015-09-14 10:18:53 +0200

    Collect all path configs in the path var

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,21 @@ var gulp = require('gulp'),
 
 // Configure paths
 var paths = {
+
+  // Inputs
   js: ['./gui/js/**/*.js'],
   css: ['./gui/css/**/*.css'],
-  images: ['./gui/i/**/*.*']
+  images: ['./gui/i/**/*.*'],
+
+  // Outputs
+  jsOutput: 'all.js',
+  cssOutput: 'all.css',
+
+  // Destinations
+  jsDest: './dist/js/',
+  cssDest: './dist/css/',
+  imgDest: './dist/i/'
+
 };
 
 // Process stylesheets
@@ -20,20 +32,20 @@ gulp.task('css', function () {
     .pipe(sourcemaps.init())
     .pipe(autoprefixer({browsers: ['last 2 versions']}))
     .pipe(postcss([customProperties()]))
-    .pipe(concat('all.css'))
+    .pipe(concat(paths.cssOutput))
     .pipe(minifyCss({compatibility: 'ie8'}))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(gulp.dest(paths.cssDest));
 });
 
 // Concatenate and minify JavaScript
 gulp.task('js', function () {
   return gulp.src(paths.js)
     .pipe(sourcemaps.init())
-    .pipe(concat('all.js'))
+    .pipe(concat(paths.jsOutput))
     .pipe(uglify())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/js/'))
+    .pipe(gulp.dest(paths.jsDest))
 });
 
 // Watch for changes in JS and CSS
@@ -45,7 +57,7 @@ gulp.task('watch', function() {
 // Copy image assets into /dist
 gulp.task('copy', function(){
   gulp.src(paths.images)
-    .pipe(gulp.dest('dist/i/'));
+    .pipe(gulp.dest(paths.imgDest));
 });
 
 gulp.task('default', ['watch', 'css', 'js', 'copy']);
